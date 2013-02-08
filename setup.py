d10eefe88665b296e273f51cdc32268addc60161
Shrek Zhou <zgwmike@hotmail.com>
2013-02-09 02:32:37 +0800

    fix setup file problem.

--- setup.py
+++ setup.py
@@ -7,21 +7,31 @@ def read(fname):
 
 setup(
     name="django-rss-plugin",
-    version="0.0.2",
+    version="0.0.3",
     author="Guangwen Zhou",
     author_email="zgwmike@hotmail.com",
-    description=("A Django CMS plugin to show django-rss-plugin."),
+    description="A Django CMS plugin to show a list of feeds.",
     license="BSD",
     keywords="django cms plugin django-rss-plugin",
     url="http://github.com/zgwmike/django-rss-plugin",
     packages=["rssplugin"],
     classifiers=[
-        'Development Status :: 3 - Alpha',
-        "Framework :: Django",
-        "Environment :: Web Environment",
-        "License :: OSI Approved :: BSD License"
+	'Development Status :: 5 - Production/Stable',
+        'Environment :: Plugins',
+        'Environment :: Web Environment',
+        'Framework :: Django',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: BSD License',
+        'Natural Language :: English',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2',
+        'Topic :: Multimedia :: Video :: Display',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+        'Topic :: Utilities',
     ],
-    install_requires=['feedparser', 'django-cms'],
-    download_url='https://github.com/zgwmike/django-rss-plugin/archive/master.zip',
+    install_requires=[
+	    'feedparser', 
+	    'Django >= 1.4',
+	    'django-cms >= 2.3'],
     long_description=read("README.rst")
 )
