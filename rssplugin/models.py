bb2c8357a02822c927d731a997f8302c892849dd
John Bazik <jsb@cs.brown.edu>
2015-12-14 16:31:58 -0500

    Set boolean default value for open_in_new_window for django 1.6+.

--- rssplugin/models.py
+++ rssplugin/models.py
@@ -8,7 +8,7 @@ class RSSPlugin(CMSPlugin):
     title = models.CharField(max_length=200, default="Community News", null=True,
                              help_text=_("If you specified this value, it will replace feed's title"))
     rss_url = models.CharField(max_length=512)
-    open_in_new_window = models.BooleanField()
+    open_in_new_window = models.BooleanField(default=False)
     cache_time = models.IntegerField(verbose_name=_("Cache time in seconds"))
 
     def __unicode__(self):
